d056417149291c8cd02ea856ea16b1ab59b9d372
mattedwards <the.matt.edwards@googlemail.com>
2021-01-21 22:37:53 +0000

    amend to Itinerary it can have ports test to include actual port objects

--- tests/itinerary.test.js
+++ tests/itinerary.test.js
@@ -1,12 +1,15 @@
 const Itinerary = require("../src/itinerary.js");
+const Port = require("../src/port.js");
 
 describe("Itinerary", () => {
     it("can be instantiated", () => {
         expect(new Itinerary()).toBeInstanceOf(Object);
     });
 
-    it("has a ports property", () => {
-        const itinerary = new Itinerary(["a", "b"]);
-        expect(itinerary.ports).not.toBeUndefined();
+    it("can have ports", () => {
+        const dover = new Port("Dover");
+        const calais = new Port("Calais");
+        const itinerary = new Itinerary([dover, calais]);
+        expect(itinerary.ports).toEqual([dover, calais]);
     });
 });
